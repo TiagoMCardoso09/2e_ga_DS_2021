7c939eb02ecb9b55148cfd26aa019c19ce3d75e1
TiagoMCardoso09 <86525944+TiagoMCardoso09@users.noreply.github.com>
2022-02-09 22:29:06 -0300

    upload "CalcularArrobaDoBoi"

---
+++ CalcularArrobaDoBoi/CalcularArrobaDoBoi/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace CalcularArrobaDoBoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRendimento = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lbValor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(424, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Insira o peso do animal (em kilos):";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeso.Location = new System.Drawing.Point(30, 64);
+            this.txtPeso.MaxLength = 10;
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(573, 26);
+            this.txtPeso.TabIndex = 0;
+            this.txtPeso.Text = "0";
+            this.txtPeso.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPeso_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(376, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Insira o valor do @ (em reais):";
+            // 
+            // txtValor
+            // 
+            this.txtValor.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(30, 147);
+            this.txtValor.MaxLength = 7;
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(573, 26);
+            this.txtValor.TabIndex = 1;
+            this.txtValor.Text = "0";
+            this.txtValor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValor_KeyPress_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(543, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Qual o rendimento de carcaça previsto (em %):";
+            // 
+            // txtRendimento
+            // 
+            this.txtRendimento.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtRendimento.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRendimento.Location = new System.Drawing.Point(30, 235);
+            this.txtRendimento.Name = "txtRendimento";
+            this.txtRendimento.Size = new System.Drawing.Size(573, 26);
+            this.txtRendimento.TabIndex = 2;
+            this.txtRendimento.Text = "0";
+            this.txtRendimento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRendimento_KeyPress_1);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.Black;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.Lime;
+            this.btnCalcular.Location = new System.Drawing.Point(30, 281);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(573, 33);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "CALCULAR O VALOR ESPERADO DO ANIMAL";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Black;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.Red;
+            this.btnLimpar.Location = new System.Drawing.Point(30, 327);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(573, 33);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "LIMPAR OS DADOS";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lbValor
+            // 
+            this.lbValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbValor.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lbValor.Location = new System.Drawing.Point(27, 391);
+            this.lbValor.Name = "lbValor";
+            this.lbValor.Size = new System.Drawing.Size(576, 69);
+            this.lbValor.TabIndex = 5;
+            this.lbValor.Text = "R$ 00,00";
+            this.lbValor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(630, 490);
+            this.Controls.Add(this.lbValor);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtRendimento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.LightGray;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Calcular @ do boi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRendimento;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lbValor;
+    }
+}
+
---
+++ CalcularArrobaDoBoi/CalcularArrobaDoBoi/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcularArrobaDoBoi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPeso.Text = "0";
+            txtRendimento.Text = "0";
+            txtValor.Text = "0";
+            lbValor.Text = "R$ 00,00";
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+        }
+
+        private void txtPeso_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+            if (c == ',') c = '.';
+            if (!char.IsNumber(c) && c != '.') e.Handled = true;
+            else e.Handled = false;
+        }
+
+        private void txtValor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+            if (c == ',') c = '.';
+            if (!char.IsNumber(c) && c != '.') e.Handled = true;
+            else e.Handled = false;
+        }
+
+        private void txtValor_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+            if (c == ',') c = '.';
+            if (!char.IsNumber(c) && c != '.') e.Handled = true;
+            else e.Handled = false;
+        }
+
+        private void txtRendimento_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+            if (c == ',') c = '.';
+            if (!char.IsNumber(c) && c != '.') e.Handled = true;
+            else e.Handled = false;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double peso = double.Parse(txtPeso.Text);
+            double rc = double.Parse(txtRendimento.Text);
+            double valor = double.Parse(txtValor.Text);
+
+            double arroba = 0;
+            double precofinal = 0;
+
+            arroba = (peso * (rc / 100))/15;
+            precofinal = arroba * valor;
+
+            Convert.ToString(precofinal);
+
+            lbValor.Text = "R$ " + precofinal.ToString("F2");
+        }
+    }
+}
